4689e1d09d41fab59c1e3cda7fac26406a6431ac
AhmedGamerGRR <45241183+AhmedGamerGRR@users.noreply.github.com>
2020-04-14 17:05:16 +0100

    Update kmusic.js

--- kmusic.js
+++ kmusic.js
@@ -3,12 +3,6 @@ const express = require('express');
 const app = express();
 app.get("/", (request, response) => {
   response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://kahrbaamcx.glitch.me/`);
-}, 280000);
- 
 
 const Discord = require('discord.js');
 const converter = require('number-to-words');
